461200a075deef3ca590ac163c46b7f9d0eb2ccc
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-09-25 14:19:57 +0200

    feat: add simple header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,22 @@
+import { InfinityIcon, Menu, User } from "lucide-react";
+import Link from "next/link";
+import colors from "tailwindcss/colors";
+
+export function Header() {
+  return (
+    <header className="fixed top-0 z-50 bg-transparent w-full py-8 bg-red-500 flex items-center justify-between px-12">
+      <Menu color="white" />
+      <Link
+        href="/"
+        className="text-4xl text-white uppercase font-bold flex items-center gap-x-3"
+      >
+        <InfinityIcon color="white" />
+        Loop
+        <InfinityIcon color="white" />
+      </Link>
+      <Link href="/sign-in">
+        <User color={colors.white} />
+      </Link>
+    </header>
+  );
+}
