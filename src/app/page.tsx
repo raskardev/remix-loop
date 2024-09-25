1113d23dcee9c29253376164751feb9cbde02d35
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-09-25 13:47:29 +0200

    feat: add index content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <main className="min-h-dvh grid grid-cols-1 md:grid-cols-2">
+      <Link
+        href="/woman"
+        className="relative flex items-center justify-center group overflow-hidden"
+      >
+        <Image
+          src="/woman.jpg"
+          alt="Woman"
+          fill
+          className="object-cover group-hover:scale-105 group-hover:rotate-2 duration-300"
+        />
+        <div className="absolute bg-black/20 inset-0 group-hover:bg-transparent duration-300" />
+        <h2 className="text-7xl text-white z-10 group-hover:underline group-hover:underline-offset-8 group-hover:decoration-1">
+          Woman
+        </h2>
+      </Link>
+      <Link
+        href="/man"
+        className="relative flex items-center justify-center group overflow-hidden"
+      >
+        <Image
+          src="/man.jpg"
+          alt="Man"
+          fill
+          className="object-cover group-hover:scale-105 group-hover:rotate-2 duration-300"
+        />
+        <div className="absolute bg-black/20 inset-0 group-hover:bg-transparent duration-300" />
+
+        <h2 className="text-7xl text-white z-10 group-hover:underline group-hover:underline-offset-8 group-hover:decoration-1">
+          Man
+        </h2>
+      </Link>
+    </main>
+  );
 }
