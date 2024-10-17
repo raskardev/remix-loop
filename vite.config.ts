8f5d4acc92d912f3d7e3bdb38941eef22c769793
raskardev <alexcdev95@gmail.com>
2024-10-17 16:14:07 +0200

    chore: add tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
@@ -19,5 +20,6 @@ export default defineConfig({
         unstable_optimizeDeps: true,
       },
     }),
+    tsconfigPaths(),
   ],
 });
